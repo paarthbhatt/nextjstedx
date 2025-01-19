8ff44448f5a67aebc4f0bb018d9294ac4e0ecbc1
SujalKishore <131854159+SujalKishore@users.noreply.github.com>
2025-01-19 16:08:49 +0530

    ticket fix

--- src/components/Ticket/Ticket.tsx
+++ src/components/Ticket/Ticket.tsx
@@ -1,51 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import { useGSAP } from '@gsap/react';
-import { Mona_Sans } from 'next/font/google';
-import gsap from 'gsap';
-import './fontArray';
-import fontArray from './fontArray';
+import React, { useState, useEffect } from "react";
+import { useGSAP } from "@gsap/react";
+import { Mona_Sans } from "next/font/google";
+import gsap from "gsap";
+import "./fontArray";
+import fontArray from "./fontArray";
 
 const mona = Mona_Sans({
-  subsets:['latin']
-})
+  subsets: ["latin"],
+});
 
 const Ticket = () => {
   useGSAP(() => {
-      gsap.to('#ticketasset', {
-           x: 100,
-          duration: 0.75,
-          opacity: 1
-      })
-  }, [])
-  const [fonts, setFonts] = useState(mona)
+    gsap.to("#ticketasset", {
+      x: 100,
+      duration: 0.75,
+      opacity: 1,
+    });
+  }, []);
+  const [fonts, setFonts] = useState(mona);
   useEffect(() => {
-    let list = fontArray
+    let list = fontArray;
     for (let font = 0; font < list.length; font++) {
-      setFonts;
+      setFonts(list[font]);
     }
-  }, [])
+  }, [fonts]);
   return (
     <div
       className="h-screen bg-cover bg-center"
-      style={{ backgroundImage: "url('/images/Ticket Page bg.png')" }}>
+      style={{ backgroundImage: "url('/images/Ticket Page bg.png')" }}
+    >
       <div className="content flex gap-5 h-full mt-20">
-        <div className="border-2 border-white w-[25%] h-[80%] opacity-0 bg-red-700" id='ticketasset'>
-            heya bitches... here is ya ticket
+        <div
+          className="border-2 border-white w-[25%] h-[80%] opacity-0 bg-red-700"
+          id="ticketasset"
+        >
+          heya bitches... here is ya ticket
         </div>
         <div className="content w-{75%} ml-40 text-white">
-            <div className=" heading pt-36 font-bold text-6xl">
-                <span className='text-[#eb0028]'>Leisure </span> 
-                <span className={fonts.className}>Experience</span>
-            </div>
-            <div className="perks">
-                <ul className='list-disc ml-24 text-xl font-semibold'>
-                    <li className='mb-3 mt-8'>Perk 1</li>
-                    <li className='mb-3 mt-3'>Perk 2</li>
-                    <li className='mb-3 mt-3'>Perk 3</li>
-                    <li className='mb-3 mt-3'>Perk 4</li>
-                    <li className='mt-3'>Perk 5</li>
-                </ul>
-            </div>
+          <div className=" heading pt-36 font-bold text-6xl">
+            <span className="text-[#eb0028]">Leisure </span>
+            {/* {fontArray.map((i) => (
+              <span className={i.className}>Hello</span>
+            ))} */}
+            <span className={fonts.className}>Experience</span>
+          </div>
+          <div className="perks">
+            <ul className="list-disc ml-24 text-xl font-semibold">
+              <li className="mb-3 mt-8">Perk 1</li>
+              <li className="mb-3 mt-3">Perk 2</li>
+              <li className="mb-3 mt-3">Perk 3</li>
+              <li className="mb-3 mt-3">Perk 4</li>
+              <li className="mt-3">Perk 5</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Ticket/fontArray.tsx
+++ src/components/Ticket/fontArray.tsx
@@ -1,46 +1,43 @@
-import { Mona_Sans } from 'next/font/google';
-import { Montserrat } from 'next/font/google';
-import { Playwrite_AU_SA } from 'next/font/google';
-import { JetBrains_Mono } from 'next/font/google';
-import { Doto } from 'next/font/google';
-import { Playfair_Display } from 'next/font/google';
-import { Nunito } from 'next/font/google';
-import { Comic_Neue } from 'next/font/google';
+import { Mona_Sans } from "next/font/google";
+import { Montserrat } from "next/font/google";
+import { Playwrite_AU_SA } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
+import { Doto } from "next/font/google";
+import { Playfair_Display } from "next/font/google";
+import { Nunito } from "next/font/google";
+import { Comic_Neue } from "next/font/google";
 
 const mona = Mona_Sans({
-    subsets:['latin']
-})
+  subsets: ["latin"],
+});
 
 const mont = Montserrat({
-    subsets:['latin']
-})
+  subsets: ["latin"],
+});
 
-const playwrite = Playwrite_AU_SA({})
+const playwrite = Playwrite_AU_SA({});
 
 const JB = JetBrains_Mono({
-    subsets:['latin']
-})
+  subsets: ["latin"],
+});
 
 const doto = Doto({
-    subsets:['latin']
-})
+  subsets: ["latin"],
+});
 
 const playfair = Playfair_Display({
-    subsets:['latin']
-})
+  subsets: ["latin"],
+});
 
 const nun = Nunito({
-    subsets:['latin']
-})
+  subsets: ["latin"],
+});
 
 const comic = Comic_Neue({
-    weight:"700",
-    subsets:['latin'],
-})
+  weight: "700",
+  subsets: ["latin"],
+});
 
-const fontArray = () => {
-  const Farray = [mona, mont, playwrite, JB, doto, playfair, nun, comic]
-  return (Farray)
-}
+const fontArray = [mona, mont, playwrite, JB, doto, playfair, nun, comic];
 
-export default fontArray
+export default fontArray;
